a066d2bce8d44bd2d034a33a2820d5e1110e7011
Joseph Venuto <jbvenuto@gmail.com>
2019-07-03 15:59:34 -0400

    Styled the app links and put in a divider

--- src/components/SlideLinks.js
+++ src/components/SlideLinks.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { MdBrightness1 } from 'react-icons/md'
 
 const SlideLinks = (props) => (
     <div className="app-links justify-content-center">
         <a href={props.liveHref} target="_blank" rel="noopener noreferrer" className="app-link">Live Site</a>
+        <MdBrightness1 className="link-divider" />
         <a href={props.gitHubHref} target="_blank" rel="noopener noreferrer" className="app-link">GitHub Repository</a>
     </div>
 );
