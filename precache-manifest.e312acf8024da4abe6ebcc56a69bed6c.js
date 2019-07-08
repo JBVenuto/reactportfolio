d77abc11e1eae504ed8299ba372835cdfac23520
Joseph Venuto <jbvenuto@gmail.com>
2019-07-08 11:25:18 -0400

    Updates

---
+++ precache-manifest.e312acf8024da4abe6ebcc56a69bed6c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881fd3a60af4295ea965a8e09c6f8590",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "9d161d2ee860d04752f6",
+    "url": "/reactportfolio/static/css/main.2a119b42.chunk.css"
+  },
+  {
+    "revision": "826877a853fce6dca4d7",
+    "url": "/reactportfolio/static/js/2.2688e769.chunk.js"
+  },
+  {
+    "revision": "9d161d2ee860d04752f6",
+    "url": "/reactportfolio/static/js/main.74d3abcd.chunk.js"
+  },
+  {
+    "revision": "8007ccf6b0ab45eb369a",
+    "url": "/reactportfolio/static/js/runtime~main.d7794af4.js"
+  },
+  {
+    "revision": "6ff14c317ef02a194e521f622358059e",
+    "url": "/reactportfolio/static/media/artgendesk.6ff14c31.gif"
+  },
+  {
+    "revision": "ed7e17a8897f65ed24919c9476b3bb94",
+    "url": "/reactportfolio/static/media/arthistory.ed7e17a8.jpg"
+  },
+  {
+    "revision": "2476f69094365b582a89f09b7e0d1f03",
+    "url": "/reactportfolio/static/media/headshotpresq.2476f690.jpg"
+  },
+  {
+    "revision": "de615b30039aced29f92892db342da38",
+    "url": "/reactportfolio/static/media/lightsdesk.de615b30.gif"
+  },
+  {
+    "revision": "edb270b59e7ac6fe19f0d31d2029629e",
+    "url": "/reactportfolio/static/media/memorydesk.edb270b5.gif"
+  },
+  {
+    "revision": "dfdf22e0ac7f5128d34dd94d9ffcf7c2",
+    "url": "/reactportfolio/static/media/modernart.dfdf22e0.jpg"
+  },
+  {
+    "revision": "58a30baae308d9087643f65c16a96bc8",
+    "url": "/reactportfolio/static/media/northernlights.58a30baa.jpg"
+  },
+  {
+    "revision": "4eac0b4618ae9f412a165a52e11a0868",
+    "url": "/reactportfolio/static/media/restaurantdesk.4eac0b46.gif"
+  },
+  {
+    "revision": "fb840a9de25df086c72e74f197ad7945",
+    "url": "/reactportfolio/static/media/restaurants.fb840a9d.jpg"
+  },
+  {
+    "revision": "04746ed753901db03c5905ead33495f7",
+    "url": "/reactportfolio/static/media/v1.04746ed7.jpg"
+  }
+]);
