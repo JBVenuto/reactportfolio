61e004981e34cb869a54b9726012cf715e2b9694
Joseph Venuto <jbvenuto@gmail.com>
2019-07-18 09:06:03 -0400

    Moved BrowserRouter from index.js to App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Lights from './pages/Lights';
 import Memory from './pages/Memory';
 
 const App = () => (
-  // <BrowserRouter>
+  <BrowserRouter>
     <div>
       <Switch>
         <Route exact path="/" component={Main} />
@@ -18,7 +18,7 @@ const App = () => (
         <Route exact path="/artgenerator" component={ArtGenerator} />
       </Switch>
     </div>
-  // </BrowserRouter>
+  </BrowserRouter>
 )
 
 export default App;
