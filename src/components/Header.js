09014b6595f5e6ea6b464fdc1e71319db2a18874
Joseph Venuto <jbvenuto@gmail.com>
2020-06-09 08:00:29 -0400

    removed comments

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,22 @@
 import React from 'react';
 
 const Header = (props) => (
-    // <div className="container">
-        // <div className="row header">
-        <div className="header">
-            {/* <div className="col-sm-1"></div> */}
-            {/* <div className="col-sm-4 name-title"> */}
-            <div className="name__wrap">
-                <div className="name-title">
-                    <h1>Joseph Venuto</h1><hr></hr>
-                    <h2>Web Developer</h2>
-                </div>
-            </div>
-            {/* <div className="col-sm-1"></div> */}
-
-            <div className="logo">
-            {/* <div className="col-sm-6 logo"> */}
-                <img
-                    className="logo-img"
-                    src={props.src}
-                    alt="V Logo"
-                ></img>
+    <div className="header">
+        <div className="name__wrap">
+            <div className="name-title">
+                <h1>Joseph Venuto</h1><hr></hr>
+                <h2>Web Developer</h2>
             </div>
         </div>
-    // </div>
+
+        <div className="logo">
+            <img
+                className="logo-img"
+                src={props.src}
+                alt="V Logo"
+            ></img>
+        </div>
+    </div>
 );
 
 export default Header;
